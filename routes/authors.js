018b1ed337152f5527082f3a48b230890ccb0776
VolodymyR <vnaichuk@gmail.com>
2023-03-02 16:42:28 +0100

    Initial Author routes

---
+++ routes/authors.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
+
+
+
+// All Authors Route
+router.get('/', async (req,res)=> {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            authors: authors, 
+            searchOptions:req.query.name 
+        })
+    } catch {
+        res.redirect('/')
+    }
+
+    //res.send('Hello world.')
+    
+})
+
+
+//New Author Route
+router.get('/new', (req,res)=> {
+    //res.send('Hello world.')
+    res.render('authors/new', {author: new Author() })
+})
+
+//Create Author Route
+router.post('/', async (req,res)=> {
+    //res.send('Hello world.')
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+
+        const newAuthor = await author.save()
+        res.redirect('authors')
+
+    } catch {
+        res.render('authors/new',{
+            author: author,
+            errorMessage: 'Error creating Author'
+        })
+    }
+
+    
+    // author.save((err, newAuthor) => {
+    //     if (err) {
+    //         res.render('authors/new', {
+    //             author: author,
+    //             errorMesage: 'Error creating Author'
+    //         })
+    //     } else {
+    //         //res.redirect(`authors/${newAuthor.id}`)
+    //         res.redirect('/authors')
+    //     }
+    // })
+
+    // author.save().then(() => {
+    //     res.redirect('/authors')
+    // }).catch((err) => {
+    //     console.log(err)
+    // })
+    //res.send(req.body.name)
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -5,14 +5,17 @@ if (process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views',__dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL,{
@@ -23,5 +26,12 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
+
+//New Line
+//MongoDB
+//user 
+//tHtk9us3nB5Pu9pH
+//mongodb+srv://user:tHtk9us3nB5Pu9pH@cluster0.oy7bive.mongodb.net/?retryWrites=true&w=majority
